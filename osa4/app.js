b13eacdf21fcb98e9d3a096e33929218d41b17ed
Harri <harrihrekola@gmail.com>
2022-08-09 07:58:03 +0300

    4.15

--- osa4/app.js
+++ osa4/app.js
@@ -1,9 +1,11 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const config = require('./utils/config')
 const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
@@ -15,6 +17,8 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
