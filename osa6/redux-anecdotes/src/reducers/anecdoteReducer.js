ee9d4efe1e94cec47215c2ce70b26da37c62395e
Harri <harrihrekola@gmail.com>
2022-09-16 17:22:33 +0300

    6.15

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdotesAtStart = []
 
@@ -40,4 +41,12 @@ const anecdoteSlice = createSlice({
 })
 
 export const { addAnecdote, votedAnecdote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
 export default anecdoteSlice.reducer
