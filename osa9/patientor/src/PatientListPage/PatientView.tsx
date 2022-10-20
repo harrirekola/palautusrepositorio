5ac5e58a5dcf22e51be4b129765d307dfbb1f334
Harri <harrihrekola@gmail.com>
2022-10-20 09:49:26 +0300

    9.17

---
+++ osa9/patientor/src/PatientListPage/PatientView.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { apiBaseUrl } from "../constants";
+import { useParams } from "react-router-dom";
+import { Patient } from "../types";
+import { useEffect } from "react";
+import { useStateValue } from "../state";
+
+const PatientView = () => {
+
+    const [{ patients }, dispatch] = useStateValue();
+    const { id } = useParams<{ id: string}>();
+
+    useEffect(() => {
+        const fetchPatient = async () => {
+            if (!id) {
+                throw new Error("Incorrect ID");
+            }
+            try {
+                const { data: patientFromApi} = await axios.get<Patient>(
+                    `${apiBaseUrl}/patients/${id}`
+                );
+                dispatch({ type: "ADD_PATIENT", payload: patientFromApi });
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        void fetchPatient();        
+    }, [dispatch]);
+
+    if (id == undefined || patients[id] == undefined) {
+        return (
+          <div>error</div>
+        );
+    }
+    const patient = (patients[id]);
+
+    return (
+        <div>
+            <h2>{patient.name}</h2>
+            <p>ssn: {patient.ssn}</p>
+            <p>occupation: {patient.occupation}</p>
+        </div>
+    );
+};
+
+export default PatientView;
